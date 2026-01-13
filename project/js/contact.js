a219f24f98f588acb38b0bf45b9e4e1046c13994
Natsu-webd <67774998+Natsu-webd@users.noreply.github.com>
2026-01-13 21:32:59 +0900

    Add files via upload

---
+++ project/js/contact.js
@@ -0,0 +1,10 @@
+    const form = document.getElementById("reservationForm");
+    form.addEventListener("submit", () => {
+      const last = localStorage.getItem("lastSubmit");
+      if (last && Date.now() - last < 60000) {
+        alert("送信間隔を空けてください。");
+        event.preventDefault();
+        return;
+      }
+      localStorage.setItem("lastSubmit", Date.now());
+    });
